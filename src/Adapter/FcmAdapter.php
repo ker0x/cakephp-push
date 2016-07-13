0dcfc26870bb68e25082fc547bc3d9d9e12eab1d
Romain <monteil.romain@gmail.com>
2016-07-13 12:14:46 +0200

    fix DocBlock warning

--- src/Adapter/FcmAdapter.php
+++ src/Adapter/FcmAdapter.php
@@ -86,7 +86,13 @@ class FcmAdapter implements AdapterInterface
     }
 
     /**
-     * @inheritdoc
+     * Send a downstream message to one or more devices.
+     *
+     * @param  mixed $ids Devices'ids
+     * @param  array $payload The notification and/or some datas
+     * @param  array $parameters Parameters for the request
+     *
+     * @return bool
      */
     public function send($ids, array $payload, array $parameters)
     {
@@ -112,7 +118,9 @@ class FcmAdapter implements AdapterInterface
     }
 
     /**
-     * @inheritdoc
+     * Return the response of the push
+     *
+     * @return string
      */
     public function response()
     {
